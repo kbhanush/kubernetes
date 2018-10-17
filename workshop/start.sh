8ae07e3da41525d3ba2e36b22baa8f65160647d5
schmidtp0740 <schmidtp0740@gmail.com>
2018-10-17 14:15:43 -0500

    changed to prod docker run command

--- workshop/start.sh
+++ workshop/start.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 docker build -t worker . &&
-docker run -it -v /var/run/docker.sock:/var/run/docker.sock -v `pwd`/database:/opt/oracle/database worker /bin/bash
+# Production run on Mac
+docker run -it -v /var/run/docker.sock:/var/run/docker.sock -v `pwd`/database:/opt/oracle/app/database worker /bin/bash
+
+# Production run on Windows
+# TODO
+
+# Dev Run
+# docker run -it -v /var/run/docker.sock:/var/run/docker.sock -v `pwd`:/opt/oracle/app worker /bin/bash
