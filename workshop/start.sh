5f0f1d47ab9570cdaa537c39a3054a7d30ea2226
schmidtp0740 <schmidtp0740@gmail.com>
2018-10-17 10:03:52 -0500

    added database folder to volume in docker run script

--- workshop/start.sh
+++ workshop/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build -t worker . &&
-docker run -it -v /var/run/docker.sock:/var/run/docker.sock  -v `pwd`:/opt/oracle worker /bin/bash
+docker run -it -v /var/run/docker.sock:/var/run/docker.sock -v `pwd`/database:/opt/oracle/database worker /bin/bash
