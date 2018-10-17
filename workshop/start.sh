d4a04f0c42eec25750b8b4419b9ef2e072ec5cd8
schmidtp0740 <schmidtp0740@gmail.com>
2018-10-17 15:27:34 -0500

    updated start.sh

--- workshop/start.sh
+++ workshop/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker build -t worker . &&
 # Production run on Mac
-docker run -it -v /var/run/docker.sock:/var/run/docker.sock -v `pwd`/database:/opt/oracle/app/database worker /bin/bash
+docker run -it -v /var/run/docker.sock:/var/run/docker.sock -v `pwd`/database/wallet:/opt/oracle/app/database/wallet worker /bin/bash
 
 # Production run on Windows
 # TODO
