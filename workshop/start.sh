a9493a2db38ae188fa9a6f65f8aca0e76a14c06d
schmidtp0740 <schmidtp0740@gmail.com>
2018-11-06 14:52:00 -0600

    optimized start script

--- workshop/start.sh
+++ workshop/start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 docker build -t worker . &&
 # Production run on Mac
-docker run -it -v /var/run/docker.sock:/var/run/docker.sock -v `pwd`/database/wallet:/opt/oracle/database/wallet worker /bin/bash
+# change database/wallet to database
+docker run -it -v /var/run/docker.sock:/var/run/docker.sock worker /bin/bash
 
 # Production run on Windows
 # TODO
 
 # Dev Run
-# docker run -it -v /var/run/docker.sock:/var/run/docker.sock -v `pwd`:/opt/oracle/app worker /bin/bash
+# docker run -it -v /var/run/docker.sock:/var/run/docker.sock -v `pwd`:/opt/oracle worker /bin/bash
